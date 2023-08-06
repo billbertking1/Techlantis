599518eea9d5caa80f39dac263b0dc5cd099e3b3
billbertking1 <billbertking1@users.noreply.github.com>
2023-08-06 00:10:49 -0500

    added atm recipes

---
+++ kubejs/server_scripts/mods/allthemodium/recipes.js
@@ -0,0 +1,43 @@
+ServerEvents.recipes(event => {
+	event.shaped('allthemodium:alloy_sword', [
+		' I ',
+		' I ',
+		' E ',
+		], {
+		E: 'allthemodium:allthemodium_rod',
+		I: 'allthemodium:unobtainium_vibranium_alloy_ingot'
+	})
+})
+
+ServerEvents.recipes(event => {
+	event.shaped('allthemodium:vibranium_allthemodium_alloy_ingot', [
+		'UIU',
+		'UIU',
+		'UIU',
+		], {
+		U: 'allthemodium:vibranium_block',
+		I: 'allthemodium:allthemodium_block'
+	})
+})
+
+ServerEvents.recipes(event => {
+	event.shaped('allthemodium:unobtainium_vibranium_alloy_ingot', [
+		'UIU',
+		'UIU',
+		'UIU',
+		], {
+		U: 'allthemodium:unobtainium_block',
+		I: 'allthemodium:vibranium_block'
+	})
+})
+
+ServerEvents.recipes(event => {
+	event.shaped('allthemodium:unobtainium_allthemodium_alloy_ingot', [
+		'UIU',
+		'UIU',
+		'UIU',
+		], {
+		U: 'allthemodium:unobtainium_block',
+		I: 'allthemodium:allthemodium_block'
+	})
+})
