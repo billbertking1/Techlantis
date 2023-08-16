2841e8d5906eadee8ec5fdcd79eebe299f1a0c7e
billbertking1 <billbertking1@users.noreply.github.com>
2023-08-16 18:49:25 -0500

    uncondensed

---
+++ kubejs/server_scripts/mods/allthemodium/allthemodium_alloy_sword.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+	event.recipes.createMechanicalCrafting('allthemodium:alloy_sword', [
+		' I ',
+		' I ',
+		' E '
+		], {
+			E: 'allthemodium:allthemodium_rod',
+			I: 'allthemodium:unobtainium_vibranium_alloy_ingot'
+		}
+})
---
+++ kubejs/server_scripts/mods/allthemodium/unobtainium_vibranium_alloy_ingot.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+	event.recipes.createMechanicalCraft('allthemodium:unobtainium_vibranium_alloy_ingot', ['CPI'], {
+		C: 'allthemodium_vibranium_ingot',
+		P: 'mekanism:pellet_antimatter',
+		I: 'allthemodiumunobtainium_ingot'
+	})
+})
