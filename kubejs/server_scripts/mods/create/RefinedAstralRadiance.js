51637e2816b0220bf1a297f7b9b6c44ffb97e445
TimeSalamander <112827502+TheRealEzufIII@users.noreply.github.com>
2023-08-18 12:48:15 -0400

    changed bug

--- kubejs/server_scripts/mods/create/RefinedAstralRadiance.js
+++ kubejs/server_scripts/mods/create/RefinedAstralRadiance.js
@@ -1,14 +1,19 @@
 ServerEvents.recipes(e => {
-  e.recipes.createMechanicalCrafting('create:handheld_worldshaper', [
-    'CPU',
-    'RIP',
-    'ORC'
+  e.recipes.createMechanicalCrafting('kubejs:refined_astral_radiance', [
+    'AAAAAAA',
+    'BCB',
+    'DCBCD',
+    'EFHFE',
+    'GEFEG',
+    'GEG'
   ], {
-    C: 'create:andesite_alloy',
-    P: 'minecraft:prismarine_crystals',
-    R: 'mekanism:pellet_antimatter',
-    I: 'allthemodium:unobtainium_allthemodium_alloy_ingot',
-    O: 'create:extendo_grip',
-    U: 'minecraft:end_crystal'
+    A:'mekanism:pellet_antimatter',
+    B:'silentgear:starmetal_dust',
+    C:'powah:crystal_nitro',
+    D:'powah:charged_snowball',
+    E:'rftoolsbase:dimensionalshard',
+    F:'minecraft:nether_star',
+    G:'powah:ender_core',
+    H:'projectexpansion:final_star_shard'
   })
 })
